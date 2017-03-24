0de3975b9e9b97883f34212e2f02e7ee68231b46
Arif Balik <arifbalik@outlook.com>
2017-03-24 14:39:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-#define __EXIT(status) return ##status;
+#define __EXIT1(cmd) printf(#cmd)
+#define __EXIT0(cmd) (strlen(cmd) ? 0 : 1)
+#define __EXIT(s) return __EXIT##s("Hello World!");
+
 
 int main(){
+    
+  __EXIT(1)
   
-  printf("Hello World!");
-  
-  return __EXIT(1);
 }
